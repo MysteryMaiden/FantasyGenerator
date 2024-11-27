da3228783da89bf1f1cc686bfb8955244ac49671
MysteryMaiden <sageriley09@icloud.com>
2024-11-27 10:02:30 -0500

    Create logStats.js

---
+++ logStats.js
@@ -0,0 +1,4 @@
+import { maleOrFemale, generatedName, generate } from './generator.js';
+// You can now use maleOrFemale and generatedName in this file
+console.log(`Imported Name: ${generatedName}`);
+console.log(`Imported Gender: ${maleOrFemale}`);
