fa91c147487dc35a3230965cee60e43fae093a50
MysteryMaiden <sageriley09@icloud.com>
2024-11-27 14:29:34 -0500

    Delete logStats.js

--- logStats.js
+++
@@ -1,6 +0,0 @@
-import { maleOrFemale, generatedName, generate } from './generator.js';
-// You can now use maleOrFemale and generatedName in this file
-console.log(`Imported Name: ${generatedName}`);
-console.log(`Imported Gender: ${maleOrFemale}`);
-
-import {rolls} from './Rolls.js'
