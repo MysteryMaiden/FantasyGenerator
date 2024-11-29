8fb439ca0b7e7ea01d8f93ed28b6f24b02b10e5f
MysteryMaiden <sageriley09@icloud.com>
2024-11-29 15:02:30 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,9 +1,7 @@
 angular.module('generApp', [])
 .controller('generateController', function() {
   var G = this;
-  G.todos = [
-    {text:'learn AngularJS', done:true},
-    {text:'build an AngularJS app', done:false}];
-g.nameG = true;
+  
+g.nameG = "true";
   
 });
