a5de68c78274072f7a84f375e8a30db5cc206658
MysteryMaiden <sageriley09@icloud.com>
2024-11-29 15:15:26 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,17 @@
+
+
 angular.module('generApp', [])
 .controller('generateController', function() {
   var G = this;
   
 g.nameG = true;
 
+g.name = function(event){
+if(event == "name"){
+g.nameG = false;
+  
+}
+  
+}:
+  
 });
